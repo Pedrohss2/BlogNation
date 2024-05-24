5632a03ae34d92ed3484c708739c3abe8962de6f
Pedro Henrique <pedro2266809ph@gmail.com>
2024-05-24 19:20:57 -0300

    feat: fix name senha to password in table Users

--- src/main/resources/db/migration/V1__CREATE_TABLE_USER.sql
+++ src/main/resources/db/migration/V1__CREATE_TABLE_USER.sql
@@ -2,6 +2,6 @@ CREATE TABLE Users (
     id INT NOT NULL PRIMARY KEY,
     name varchar(50),
     email varchar(150),
-    senha varchar(50),
+    password varchar(50),
     role ENUM('ADMIN', 'USER')
 );
